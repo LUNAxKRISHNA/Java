f12f41b8a56157a212dcd29cd8b48f9cbffb5c9e
LUNAxKRISHNA <krishnaolassa@gmail.com>
2025-07-11 14:09:28 +0530

    Fibonacci Series

---
+++ src/FibonacciLogical.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class FibonacciLogical {
+    static int[] fibArray;
+    public int fib(int n) {
+        if (fibArray[n] == -1) {
+            fibArray[n] = fib(n - 1) + fib(n - 2);
+        }
+        return fibArray[n];
+    }
+    public static void main(String[] args) {
+        FibonacciLogical x = new FibonacciLogical();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = scanner.nextInt();
+        fibArray = new int[n + 1];
+        for (int i = 0; i < fibArray.length; i++) {
+            fibArray[i] = -1;
+        }
+        fibArray[0] = 0;
+        if (n > 0) fibArray[1] = 1;
+        System.out.println("The number entered is: " + n);
+        System.out.println("The nth Fibonacci term is: " + x.fib(n));
+    }
+}
